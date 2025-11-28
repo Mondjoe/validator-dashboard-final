abc6735b9b148182066427198049a7077044511e
Charm <194323569+Mondjoe@users.noreply.github.com>
2025-11-28 09:42:48 +0400

    Create Dashboard.vue

---
+++ page/src/views/Dashboard.vue
@@ -0,0 +1,20 @@
+<!-- src/views/Dashboard.vue -->
+<template>
+  <main>
+    <h1>Validator Dashboard</h1>
+    <AuditTrail />
+    <SnapshotViewer />
+  </main>
+</template>
+
+<script setup>
+import AuditTrail from '../components/AuditTrail.vue'
+import SnapshotViewer from '../components/SnapshotViewer.vue'
+</script>
+
+<style>
+main {
+  padding: 2rem;
+  font-family: Arial, sans-serif;
+}
+</style>
