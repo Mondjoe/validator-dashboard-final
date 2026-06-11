632fe72259c30f05a3e452ec8fbf72000cb2ce0b
Charm <194323569+Mondjoe@users.noreply.github.com>
2026-06-12 01:44:39 +0400

    Delete hooks/useCapsuleIdentity.ts
    Signed-off-by: Charm <194323569+Mondjoe@users.noreply.github.com>

--- hooks/useCapsuleIdentity.ts
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useIdentity } from "./useIdentity";
-import { buildCapsuleIdentity } from "@/lib/identity/capsule";
-
-export function useCapsuleIdentity() {
-  const id = useIdentity();
-  return buildCapsuleIdentity(id);
-}
