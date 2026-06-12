8447a059a59ecb2740fd9d7791bcab909646a070
Charm <194323569+Mondjoe@users.noreply.github.com>
2026-06-12 04:04:01 +0400

    Update page.tsx
    Signed-off-by: Charm <194323569+Mondjoe@users.noreply.github.com>

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,9 +2,6 @@ import { ChainStatus } from '@/components/ChainStatus';
 import { OperatorAlerts } from '@/components/OperatorAlerts';
 import { CommandPalette } from '@/components/CommandPalette';
 import { RewardAnalytics } from '@/components/RewardAnalytics';
-import NftList from '@/components/NftList';
-import ContractOwnership from '@/components/ContractOwnership';
-import DaoGovernance from '@/components/DaoGovernance';
 import { VALIDATORS } from '@/lib/validators';
 
 export default function DashboardPage() {
