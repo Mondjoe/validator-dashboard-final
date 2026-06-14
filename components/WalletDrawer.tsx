8f46cc7406e9c01404fb314cb35b55c5b21d4aea
Charm <194323569+Mondjoe@users.noreply.github.com>
2026-06-14 13:41:28 +0400

    Update WalletDrawer.tsx
    Signed-off-by: Charm <194323569+Mondjoe@users.noreply.github.com>

--- components/WalletDrawer.tsx
+++ components/WalletDrawer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ConnectKitButton } from "connectkit";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useWallet as useSolanaWallet } from "@solana/wallet-adapter-react";
 import { TonConnectButton } from "@tonconnect/ui-react";
 import { useEffect, useState } from "react";
