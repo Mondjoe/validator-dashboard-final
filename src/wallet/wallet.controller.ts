abd3373ea60ac4050107a2341f4155e22c886eb2
CharmCapsule <mondjoe007@yahoo.com>
2026-07-17 07:44:52 +0400

    Update wallet.controller.ts

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -1,10 +1,24 @@
-import { Module } from '@nestjs/common';
-import { WalletController } from './wallet.controller';
+import { Body, Controller, Post } from '@nestjs/common';
 import { WalletService } from './wallet.service';
+import { ConnectWalletDto } from './dtos/connect-wallet.dto';
+import { EvmSignDto } from './dtos/evm-sign.dto';
 
-@Module({
-  controllers: [WalletController],
-  providers: [WalletService],
-  exports: [WalletService],
-})
-export class WalletModule {}
+@Controller('wallet')
+export class WalletController {
+  constructor(private readonly walletService: WalletService) {}
+
+  @Post('connect')
+  async connect(@Body() dto: ConnectWalletDto) {
+    return this.walletService.connect(dto);
+  }
+
+  @Post('evm/sign')
+  async signEvm(@Body() dto: EvmSignDto) {
+    return this.walletService.signEvm(dto);
+  }
+
+  @Post('evm/balance')
+  async evmBalance(@Body() dto: ConnectWalletDto) {
+    return this.walletService.getEvmBalance(dto.address);
+  }
+}
