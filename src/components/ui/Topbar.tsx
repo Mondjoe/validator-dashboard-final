d69adcc020f9d72565f691345bdcb21e29a85bbf
CharmCapsule <mondjoe007@yahoo.com>
2026-07-20 08:09:02 +0400

    Delete Topbar.tsx

--- src/components/ui/Topbar.tsx
+++
@@ -1,7 +0,0 @@
-export default function Topbar() {
-  return (
-    <header className="h-16 border-b border-neutral-800 flex items-center px-6 bg-neutral-950">
-      <span className="text-neutral-400">Operator Dashboard</span>
-    </header>
-  );
-}
